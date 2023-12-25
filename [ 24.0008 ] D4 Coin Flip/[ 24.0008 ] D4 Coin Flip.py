7d2ad7ab2dcac5d3ad5ad9cba68eb99f46ef743c
Gavin Martin <gavin.l.martin@gmail.com>
2023-12-24 18:11:24 -0600

    determines the result of a 50/50 coin flip

---
+++ [ 24.0008 ] D4 Coin Flip/[ 24.0008 ] D4 Coin Flip.py
@@ -0,0 +1,10 @@
+import random
+
+heads_or_tails = random.randint(0, 1)
+
+# print(heads_or_tails)
+
+if heads_or_tails == 0:
+    print("The coin is flipped. It's heads!")
+else:
+    print("The coin is flipped. It's tails!")
